ecfdf4d8560e5e512dc000873c64ee6e4e5f8e9d
Devin D'Angelo <devinjdangelo@gmail.com>
2020-05-26 14:14:53 -0400

    add animation and topn for gcn 2d embed

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,10 +1,16 @@
 python3 geosim.py
 
 python3 SageSimEmbed.py \
---num-epochs 300 --batch-size 25 \
---test-every 1 --lr .01 --n-classes 40 \
+--num-epochs 150 --batch-size 25 \
+--test-every 1 --lr .01 --n-classes 20 \
 --p-train 1 --distance-metric mse --embedding-dim 2 \
 --sup-weight 1 --neg_samples 10 --agg-type gcn
+
+python3 SageSimEmbed.py \
+--num-epochs 150 --batch-size 25 \
+--test-every 1 --lr .01 --n-classes 20 \
+--p-train 1 --distance-metric mse --embedding-dim 2 \
+--sup-weight 0 --neg_samples 1 --agg-type gcn
 
 python3 SageSimEmbed.py \
 --num-epochs 300 --batch-size 25 \
@@ -23,3 +29,16 @@ python3 SageSimEmbed.py \
 --test-every 10 --lr .01 --n-classes 10000 \
 --p-train 0.1 --distance-metric cosine --embedding-dim 16 \
 --sup-weight 0.5 --neg_samples 1 --agg-type gcn
+
+python3 SageSimEmbed.py \
+--num-epochs 100 --batch-size 1000 \
+--test-every 10 --lr .01 --n-classes 20000 \
+--p-train 0.05 --distance-metric cosine --embedding-dim 16 \
+--sup-weight 0.5 --neg_samples 1 --agg-type gcn
+
+python3 SageSimEmbed.py \
+--num-epochs 100 --batch-size 1000 \
+--test-every 10 --lr .01 --n-classes 100000 \
+--p-train 0.01 --distance-metric cosine --embedding-dim 16 \
+--sup-weight 0.5 --neg_samples 1 --agg-type gcn
+

39aaf770d9facbb311af77165b7d669ef72ee551
Devin D'Angelo <devinjdangelo@gmail.com>
2020-07-13 16:14:20 -0400

    make pip installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fastrec',
+	  version='0.0.1',
+	  description='Rapidly deployed gnn based recommender',
+	  url='https://github.com/devinjdangelo/FastRec',
+	  author='Devin DAngelo',
+	  packages=['fastrec'],
+	  zip_safe=False)

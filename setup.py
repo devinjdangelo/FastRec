e4beef1e2e5879c631d25f6693382ea150912fe3
Devin D'Angelo <devinjdangelo@gmail.com>
2020-07-17 14:44:08 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
 		return f.read()
 
 setup(name='fastrec',
-	  version='0.0.3',
+	  version='0.0.4',
 	  description='Rapidly deployed gnn based recommender',
 	  long_description=readme(),
 	  url='https://github.com/devinjdangelo/FastRec',
